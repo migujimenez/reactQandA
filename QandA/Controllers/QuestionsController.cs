daa3350b252edc31086a3bf02c758cc4ab6af87f
MigueX <migujimenez@uan.edu.co>
2021-11-09 18:13:57 -0500

    Adding controller

---
+++ QandA/Controllers/QuestionsController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QandA.Data;
+using QandA.Data.Models;
+using System.Collections.Generic;
+
+namespace QandA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        #region Questions
+        [HttpGet]
+        public IEnumerable<QuestionGetManyResponse> GetQuestions(string search)
+        {
+            if (!string.IsNullOrEmpty(search))
+            {
+                return _dataRepository.GetQuestionsBySearch(search);
+            }
+            else
+            {
+                return _dataRepository.GetQuestions();
+            }
+        }
+
+        [HttpGet("unanswered")]
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions() => _dataRepository.GetUnansweredQuestions();
+
+        [HttpGet("{questionId}")]
+        public ActionResult<QuestionGetSingleResponse> GetQuestion(int questionId)
+        {
+            var question = _dataRepository.GetQuestion(questionId);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            return Ok(question);
+        }
+
+        [HttpPost]
+        public ActionResult<QuestionGetSingleResponse> PostQuestion(QuestionPostRequest questionPostRequest)
+        {
+            var savedQuestion = _dataRepository.PostQuestion(questionPostRequest);
+            return CreatedAtAction(nameof(GetQuestion), new { questionId = savedQuestion.QuestionId }, savedQuestion);
+        }
+
+        [HttpPut("{questionId}")]
+        public ActionResult<QuestionGetSingleResponse> PutQuestion(int questionId, QuestionPutRequest questionPutRequest)
+        {
+            var question = _dataRepository.GetQuestion(questionId);
+            if (question == null)
+            { return NotFound(); }
+            else
+            {
+                questionPutRequest.Title = string.IsNullOrEmpty(questionPutRequest.Title) ? question.Title : questionPutRequest.Title;
+                questionPutRequest.Content = string.IsNullOrEmpty(questionPutRequest.Content) ? question.Content : questionPutRequest.Content;
+                var savedQuestion = _dataRepository.PutQuestion(questionId, questionPutRequest);
+                return savedQuestion;
+            }
+        }
+
+        [HttpDelete("{questionId}")]
+        public ActionResult DeleteQuestion(int questionId)
+        {
+            var question = _dataRepository.GetQuestion(questionId);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _dataRepository.DeleteQuestion(questionId);
+                return NoContent();
+            }
+        }
+
+        #endregion
+
+        #region Answers
+        [HttpPost("answer")]
+        public ActionResult<AnswerGetResponse> PostAnswer(AnswerPostRequest answerPostRequest)
+        {
+            var questionExists = _dataRepository.QuestionExists(answerPostRequest.QuestionId);
+            if(questionExists == false)
+            {
+                return NotFound();
+            }
+            var answerSaved = _dataRepository.PostAnswer(answerPostRequest);
+            return answerSaved;
+        }
+
+
+        #endregion
+
+
+    }
+}
--- QandA/Startup.cs
+++ QandA/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DbUp;
+using QandA.Data;
 
 namespace QandA
 {
@@ -33,6 +34,8 @@ namespace QandA
             }
 
             services.AddControllers();
+
+            services.AddScoped<IDataRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
