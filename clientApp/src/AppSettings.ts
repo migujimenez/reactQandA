6242f44c60a0e5991eabd46690f274e338fabcb2
Miguel Jimenez <miguel.jimenez>
2022-02-10 19:43:31 -0500

    Update Auth0 ClientID - Sucessfull Sign In Out

--- clientApp/src/AppSettings.ts
+++ clientApp/src/AppSettings.ts
@@ -4,7 +4,7 @@ export const webAPIUrl = `${server}/api`;
 
 export const authSettings = {
   domain: 'dev-gyr33wnb.us.auth0.com',
-  client_id: 'your-clientid',
+  client_id: '6W7O74JLG8XFg7NTugeWH88EUsqXu9vS',
   redirect_uri: window.location.origin + '/signin-callback',
   scope: 'openid profile QandAAPI email',
   audience: 'https://qanda',
