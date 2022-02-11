585a17935873673bdf03db215f03cf9d9c62e8c5
Miguel Jimenez <miguel.jimenez>
2022-02-11 18:11:51 -0500

    Adding back end unit tests

---
+++ BackEndTests/Controllers/QuestionsControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using QandA.Controllers;
+using QandA.Data;
+using QandA.Data.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QandA_BackEndTests.Controllers
+{
+    public class QuestionsControllerTests
+    {
+
+        [Fact]
+        public async void GetQuestions_WhenNoParameters_ReturnsAllQuestions()
+        {
+            var mockQuestions = new List<QuestionGetManyResponse>();
+            for (int i = 0; i < 10; i++)
+            {
+                mockQuestions.Add(new QuestionGetManyResponse
+                {
+                    QuestionId = 1,
+                    Title = $"Test title {i}",
+                    Content = $"Test content {i}",
+                    UserName = "User1",
+                    Answers = new List<AnswerGetResponse>()
+                });
+            }
+
+            var mockDataRepository = new Mock<IDataRepository>();
+            mockDataRepository.Setup(repo => repo.GetQuestionsAsync()).Returns(() => Task.FromResult(mockQuestions.AsEnumerable()));
+
+            var mockConfigurationRoot = new Mock<IConfigurationRoot>();
+            mockConfigurationRoot.Setup(config => config[It.IsAny<string>()]).Returns("some setting");
+
+            var questionsController = new QuestionsController(mockDataRepository.Object, null, null, mockConfigurationRoot.Object);
+
+            var result = await questionsController.GetQuestions(null, false);
+
+            Assert.Equal(10, result.Count());
+            mockDataRepository.Verify(mock => mock.GetQuestionsAsync(), Times.Once());
+        }
+
+        [Fact]
+        public async void GetQuestions_WhenHaveSearchParameter_ReturnsCorrectQuestions()
+        {
+            var mockQuestions = new List<QuestionGetManyResponse>();
+            mockQuestions.Add(new QuestionGetManyResponse
+            {
+                QuestionId = 1,
+                Title = $"Test title",
+                Content = $"Test content",
+                UserName = "User1",
+                Answers = new List<AnswerGetResponse>()
+            });
+
+            var mockDataRepository = new Mock<IDataRepository>();
+            mockDataRepository.Setup(repo => repo.GetQuestionsBySearchWithPagingAsync("Test", 1, 20)).Returns(() => Task.FromResult(mockQuestions.AsEnumerable()));
+
+            var mockConfigurationRoot = new Mock<IConfigurationRoot>();
+            mockConfigurationRoot.Setup(config => config[It.IsAny<string>()]).Returns("some setting");
+
+            var questionsController = new QuestionsController(mockDataRepository.Object, null, null, mockConfigurationRoot.Object);
+
+            var result = await questionsController.GetQuestions("Test", false);
+
+            Assert.Single(result);
+            mockDataRepository.Verify(mock => mock.GetQuestionsBySearchWithPagingAsync("Test",1,20), Times.Once());
+        }
+
+        [Fact]
+        public async void GetQuestion_WhenQuestionNotFound_Returns404()
+        {
+            var mockDataRepository = new Mock<IDataRepository>();
+            mockDataRepository.Setup(repo => repo.GetQuestionAsync(1)).Returns(()=> Task.FromResult(default(QuestionGetSingleResponse)));
+
+            var mockQuestionCache = new Mock<IQuestionCache>();
+            mockQuestionCache.Setup(cache => cache.Get(1)).Returns(() => null);
+
+            var mockConfigurationRoot = new Mock<IConfigurationRoot>();
+            mockConfigurationRoot.Setup(config => config[It.IsAny<string>()]).Returns("some setting");
+
+            var questionsController = new QuestionsController(mockDataRepository.Object, mockQuestionCache.Object, null, mockConfigurationRoot.Object);
+
+            var result = await questionsController.GetQuestion(1);
+
+            var actionResult = Assert.IsType<ActionResult<QuestionGetSingleResponse>>(result);
+            Assert.IsType<NotFoundResult>(actionResult.Result);
+        }
+    }
+}
