c00128550579e88e967838c51701f2744d5eb9ac
Miguel Jimenez <miguel.jimenez>
2022-02-15 19:17:07 -0500

    Fixing Cypress automated integration tests

--- clientApp/cypress/integration/qanda.js
+++ clientApp/cypress/integration/qanda.js
@@ -7,27 +7,27 @@ describe('Ask question', () => {
     cy.contains('Unanswered Questions');
     cy.contains('Sign In').click();
     cy.url().should('include', 'auth0');
-    cy.findByLabelText('Email')
-      .type('user_name')
-      .should('have.value', 'user_name');
+    cy.get('#username')
+      .click()
+      .get('#username')
+      .type('jimenez555@gmail.com', { force: true });
 
-    cy.findByLabelText('Password')
-      .type('password')
-      .should('have.value', 'password');
+    cy.get('#password')
+      .click()
+      .get('#password')
+      .type('EvaRosario21.', { force: true });
 
-    cy.get('form').submit();
-
+    cy.contains('Continue').click();
     cy.contains('Unanswered Questions');
 
     cy.contains('Ask a question').click();
     cy.contains('Ask a question');
-
     var title = 'title test';
     var content = 'Lots and lots and lots and lots and lots of content test';
-    cy.findByLabelText('Title').type(title).should('have.value', title);
-    cy.findByLabelText('Content').type(content).should('have.value', content);
+    cy.get('#title').type(title).should('have.value', title);
+    cy.get('#content').click().type(content).should('have.value', content);
 
-    cy.contains('Submit Your Question').click();
+    cy.contains('Submit your question').click();
     cy.contains('Your question was successfully submitted');
 
     cy.contains('Sign Out').click();
